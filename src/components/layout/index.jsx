fc6a46e1c98148dc7b0d5be1d199dfb04ec1ca1e
RahulAnandh <rahulanandh80@gmail.com>
2024-11-27 18:22:22 +0530

    Login Page

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,4 @@
+const LayoutIndex = () => {
+  return <div>LayoutIndex</div>;
+};
+export default LayoutIndex;
---
+++ src/components/login/index.jsx
@@ -0,0 +1,5 @@
+import "./login.css";
+const LoginIndex = () => {
+  return <div className="login_card"></div>;
+};
+export default LoginIndex;
