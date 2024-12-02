ce47f4723348cb393e801d741f18806bf1cb9b83
RahulAnandh <rahulanandh80@gmail.com>
2024-12-02 23:12:48 +0530

    Login UI

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
 import "./login.css";
 const LoginIndex = () => {
+  const [loginData, setLoginData] = useState({ user_name: "", password: "" });
+  const callLoginApi = () => {
+    console.log("1---1", loginData);
+  };
   return (
     <div className="body_background">
-      <div className="login_card"></div>
+      <div className="login_card">
+        <div className="login_heading">Login</div>
+        <input
+          className="login_input"
+          placeholder="Username"
+          onChange={(e) => {
+            setLoginData({ ...loginData, user_name: e.target.value });
+          }}
+        ></input>
+        <input
+          className="login_input"
+          placeholder="Password"
+          type="password"
+          onChange={(e) => {
+            setLoginData({ ...loginData, password: e.target.value });
+          }}
+        ></input>
+        <button className="login_button" onClick={callLoginApi}>
+          Login
+        </button>
+      </div>
     </div>
   );
 };
