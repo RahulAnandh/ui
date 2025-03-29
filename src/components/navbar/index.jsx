8dc97c2f61bc065c67ecffb57ab39bcabe419815
Rahul Anandh <rahulanandh80@gmail.com>
2025-03-29 15:12:01 +0530

    Changed UL List

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,21 +5,12 @@ const NavbarIndex = () => {
   return (
     <div className="navigation-bar">
       <h1><div id="red">Red</div><div id="land">Land</div></h1>
+      <hr/>
       <nav>
-        <ul>
-          <li>
             <h2><Link to="dashboard">Dashboard</Link></h2>
-          </li>
-          <li>
             <h2><Link to="home">Home</Link></h2>
-          </li>
-          <li>
             <h2><Link to="profile">Profile</Link></h2>
-          </li>
-          <li>
             <h2><Link to="employee">Employee</Link></h2>
-          </li>
-        </ul>
       </nav>
     </div>
   );
